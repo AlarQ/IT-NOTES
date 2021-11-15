00337ba4ed641c5c630898ea341ba2eae40620bb
AlarQ <bednarczykernest@gmail.com>
2021-11-15 19:26:17 +0100

    set config vars for bonsai ES

--- app/elastic/ElasticRepository.scala
+++ app/elastic/ElasticRepository.scala
@@ -3,13 +3,38 @@ package elastic
 import com.sksamuel.elastic4s.http.JavaClient
 import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties}
 import elastic.ops.{ReadOps, WriteOps}
+import org.apache.http.auth.{AuthScope, UsernamePasswordCredentials}
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
+import org.elasticsearch.client.RestClientBuilder.HttpClientConfigCallback
 
 import scala.concurrent.ExecutionContext
 
 case class ElasticRepository(props: ElasticProperties) extends WriteOps with ReadOps {
   implicit val ec: ExecutionContext = ExecutionContext.global
 
-  implicit val elasticClient = ElasticClient(
-    JavaClient(props)
-  )
+  lazy val provider = {
+    val provider = new BasicCredentialsProvider
+    val userName = sys.env("ES_USER")
+    val pass = sys.env("ES_PASS")
+    val credentials = new UsernamePasswordCredentials(userName, pass)
+    provider.setCredentials(AuthScope.ANY, credentials)
+    provider-
+  }
+
+  implicit val elasticClient = {
+    val esHost = sys.env("ES_HOST")
+    ElasticClient(
+      JavaClient(
+        ElasticProperties(esHost),
+        (requestConfigBuilder: RequestConfig.Builder) => requestConfigBuilder,
+        new HttpClientConfigCallback {
+          override def customizeHttpClient(httpClientBuilder: HttpAsyncClientBuilder) = {
+            httpClientBuilder.setDefaultCredentialsProvider(provider)
+          }
+        }
+      )
+    )
+  }
 }
